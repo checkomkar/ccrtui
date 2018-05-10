3541e3f40529b20d83a5a257c2156de8a0a7f6fb
Omkar Kamale <check.omkar@gmail.com>
2018-05-10 17:46:19 +0530

    changes

--- js/policy-details.js
+++ js/policy-details.js
@@ -11,4 +11,44 @@ $(document).ready(function () {
         }
 
     })
+    var $radioEquipmentGroup = $('input[name=equipment-break]')
+    $('input[name=equipment-break]').on('change', function (e) {
+        console.log(e);
+        if (e.target.value === '1') {
+            $('#refer-quote').modal();
+        }
+    })
+
+    $('#refer-quote').on('hidden.bs.modal', function () {
+        //$radioEquipmentGroup.prop('checked', false);
+    })
+
+    $('#cancel-refer-quote, #close-refer-quote').on('click', function (e) {
+        $radioEquipmentGroup.prop('checked', false);
+    })
+
+    $('.change-occupancy').on('click', function (e) {
+        e.preventDefault();
+        $('#selectOccupancyModal').modal();
+    })
+
+    $('.save-table').on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        console.log($this.text())
+        var innerText = $this.text()
+        if (innerText == 'Save') {
+            $this.text('Edit');
+            $this.closest('table').find('input, select').each(function () {
+                $(this).prop('disabled', true);
+            });
+        } else {
+            $this.text('Save');
+            $this.closest('table').find('input, select').each(function () {
+                $(this).prop('disabled', false);
+            });
+        }
+        
+    })
+    //$('.save-table').closest('table input').prop('disabled', true);
 })
