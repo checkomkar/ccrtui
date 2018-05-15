3a8f231bb85e191b80a35fad98807f1ef0ce8ea5
Omkar Kamale <check.omkar@gmail.com>
2018-05-15 20:53:23 +0530

    changes

--- js/policy-details.js
+++ js/policy-details.js
@@ -58,22 +58,87 @@ $(document).ready(function () {
         }
     });
 
+    var validateRadio = function () {
+        //check all radio buttons
+        if (($("input[name='equipment-break']:checked").length > 0)) {
+            // one ore more checkboxes are checked
+            $("input[name='equipment-break']").parents('.q-content').find('label.error').addClass('d-none');
+        }
+        else {
+            // no checkboxes are checked
+            $("input[name='equipment-break']").parents('.q-content').find('label.error').removeClass('d-none');
+            return false;
+        }
+        if (($("input[name='risk']:checked").length > 0)) {
+            // one ore more checkboxes are checked
+            $("input[name='risk']").parents('.q-content').find('label.error').addClass('d-none');
+        }
+        else {
+            // no checkboxes are checked
+            $("input[name='risk']").parents('.q-content').find('label.error').removeClass('d-none');
+            return false;
+        }
+        if (($("input[name='risk-spoil']:checked").length > 0)) {
+            // one ore more checkboxes are checked
+            $("input[name='risk-spoil']").parents('.q-content').find('label.error').addClass('d-none');
+        }
+        else {
+            // no checkboxes are checked
+            $("input[name='risk-spoil']").parents('.q-content').find('label.error').removeClass('d-none');
+            return false;
+        }
+        if (($("input[name='project-begun']:checked").length > 0)) {
+            // one ore more checkboxes are checked
+            $("input[name='project-begun']").parents('.q-content').find('label.error').addClass('d-none');
+        }
+        else {
+            // no checkboxes are checked
+            $("input[name='project-begun']").parents('.q-content').find('label.error').removeClass('d-none');
+            return false;
+        }
+    }
+
     $("#smartwizard").on("leaveStep", function (e, anchorObject, stepNumber, stepDirection) {
         console.log(validatorStep2);
         var selectInsuredTypeIsValid = validatorStep2.element('#select-insured-type');
         var policyLimit = validatorStep2.element('#policy-limit');
+        var selectOccupancy = validatorStep2.element('#select-occupancy');
+        var bestDescription = validatorStep2.element('#best-description');
+        var projectDuration = validatorStep2.element('#project-duration');
+        var softCost = validatorStep2.element('#soft-cost');
+        var extraExpense = validatorStep2.element('#extra-expense');
+        var startupDelay = validatorStep2.element('#startup-delay');
+        var riskLocations = validatorStep2.element('#risk-locations');
         console.log(selectInsuredTypeIsValid);
         console.log(stepNumber)
-        if(stepNumber == 1){
-            if (($("input[name='equipment-break']:checked").length > 0)) {
-                // one ore more checkboxes are checked
-            }
-            else {
-                // no checkboxes are checked
-                return false;
-            }
+
+        $('input[type="radio"]').on('change', validateRadio);
+
+        if (stepNumber == 1) {
+            //check all inputs and dropdownds
+
+            return validateRadio();
+            
         }
         
 
     })
+
+    var addLocGridRow = function () {
+        var locGridHtml = $.parseHTML($('#location-grid-row').html())[1];
+        
+        console.log(locGridHtml)
+        if (locCounter >= 10) {
+            return;
+        } else {
+            $(locGridHtml).attr("id", "table" + locCounter++);
+            $('.location-tables-section').append(locGridHtml);
+        }
+        
+    }
+    var locCounter = 1;
+    $('#add-loc-main').on('click', function () {
+        addLocGridRow();
+    })
+
 })
