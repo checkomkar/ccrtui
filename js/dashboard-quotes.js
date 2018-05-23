6548ec511b4630453b735218b3832fd6efe0b135
Omkar Kamale <check.omkar@gmail.com>
2018-05-23 18:54:02 +0530

    dhashboard changes

--- js/dashboard-quotes.js
+++ js/dashboard-quotes.js
@@ -9,15 +9,29 @@ $(document).ready(function () {
             console.log(res);
             var qoutesTable = $('#table_quotes').DataTable({
                 data: res,
-                "dom": '<"top"ip>rt<"bottom"lp><"clear">',
+                "oLanguage": {
+                    "oPaginate": {
+                        "sFirst": "<<",
+                        "sPrevious": '<i class="fa fa-arrow-left" aria-hidden="true"></i> Prev', // This is the link to the previous page
+                        "sNext": 'Next<i class="fa fa-arrow-right" aria-hidden="true"></i>', // This is the link to the next page
+                        "sLast": ">>"
+                    }
+                },
+                "dom": '<"top row"<"col-md-5"><"col-md-3"i><"col-md-4"p>>rt<"bottom row"<"col-md-5"l><"col-md-3"i><"col-md-4"p>><"clear">',
                 columns: [
                     {
                         data: null,
                         render: function (data, type, row) {
-                            var $inputRadio = '<input type="radio" name="selectRow" value="' + JSON.stringify(row) + '" />';
+                            var $inputRadio = '<div class="form-check form-check-inline">'+
+                                                '<label class="check-container">'+
+                                                '<input type="radio" name="copy">'+
+                                                '<span class="checkmark"></span>'+
+                                                '</label>'+
+                                                '</div>';
                             return $inputRadio;
                         },
-                        orderable: false
+                        orderable: false,
+                        width: '1px'
 
                     },
                     { data: 'quoteNumber' },
@@ -36,6 +50,30 @@ $(document).ready(function () {
             $('#searchTable').keyup(function () {
                 qoutesTable.search($(this).val()).draw();
             })
+            var selectedRow = null;
+            var $copyBtn = $('.copy');
+            $('#table_quotes tbody').on('click', 'tr', function () {
+                //console.log(qoutesTable.row(this).data());
+                selectedRow = qoutesTable.row(this).data();
+                var $this = $(this);
+                $this.find(':radio').prop('checked', true);
+                if ($(this).hasClass('selected')) {
+                    $(this).removeClass('selected');
+                }
+                else {
+                    qoutesTable.$('tr.selected').removeClass('selected');
+                    $(this).addClass('selected');
+                }
+                
+                $copyBtn.removeClass('d-none');
+
+            });
+
+            $copyBtn.on('click', function () {
+                console.log(selectedRow);
+            })
+            
+
         });
     }
 
