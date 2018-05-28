60d417b53f617b7642a86abb39ce2c98bee69bb9
Omkar Kamale <check.omkar@gmail.com>
2018-05-28 22:04:30 +0530

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
 
     // Smart Wizard
     $('#smartwizard').smartWizard({
-            selected: 2,
+            selected: 1,
             theme: 'default',
             transitionEffect:'fade',
             showStepURLhash: true,
