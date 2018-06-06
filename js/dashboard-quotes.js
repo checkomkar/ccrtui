18deede0432ac7e3014f1167fcc7d21259bed02a
Omkar Kamale <check.omkar@gmail.com>
2018-06-06 13:18:03 +0530

    dashboard changes

--- js/dashboard-quotes.js
+++ js/dashboard-quotes.js
@@ -82,5 +82,84 @@ $(document).ready(function () {
         });
     }
 
+    var callJson2 = function () {
+        $.ajax({
+            url: "./js/due_soon.json",
+
+        }).done(function (res) {
+            console.log(res);
+            var duesTable = $('#table_due_soon').DataTable({
+                data: res,
+                "oLanguage": {
+                    "oPaginate": {
+                        "sFirst": "<<",
+                        "sPrevious": '<i class="fa fa-arrow-left" aria-hidden="true"></i> Prev', // This is the link to the previous page
+                        "sNext": 'Next<i class="fa fa-arrow-right" aria-hidden="true"></i>', // This is the link to the next page
+                        "sLast": ">>"
+                    }
+                },
+                "dom": '<"top row"<"col-md-3"><"col-md-5"i><"col-md-4"p>>rt<"bottom row"<"col-md-5"l><"col-md-3"i><"col-md-4"p>><"clear">',
+                columns: [
+                    {
+                        data: null,
+                        render: function (data, type, row) {
+                            var $inputRadio = '<div class="form-check form-check-inline">' +
+                                '<label class="check-container">' +
+                                '<input type="radio" name="copy">' +
+                                '<span class="checkmark"></span>' +
+                                '</label>' +
+                                '</div>';
+                            return $inputRadio;
+                        },
+                        orderable: false,
+                        width: '1px'
+
+                    },
+                    { data: 'quoteNo' },
+                    { data: 'insuredName' },
+                    { data: 'expiringDays' },
+                    { data: 'status' },
+                    { data: 'createdBy' },
+                    { data: 'ccRef' },
+                    { data: 'brokerName' },
+                    { data: 'brokerPostal' },
+                    
+
+                ]
+            });
+
+            $('#searchTableDue').keyup(function () {
+                duesTable.search($(this).val()).draw();
+            })
+            var selectedRow = null;
+            var $copyBtn = $('.copy-due');
+            $('#table_due_soon tbody').on('click', 'tr', function () {
+                //console.log(qoutesTable.row(this).data());
+                selectedRow = duesTable.row(this).data();
+                var $this = $(this);
+                $this.find(':radio').prop('checked', true);
+                if ($(this).hasClass('selected')) {
+                    $(this).removeClass('selected');
+                }
+                else {
+                    duesTable.$('tr.selected').removeClass('selected');
+                    $(this).addClass('selected');
+                }
+
+                $copyBtn.removeClass('d-none');
+
+            });
+
+            $copyBtn.on('click', function () {
+                console.log(selectedRow);
+            })
+
+
+        });
+    }
+
     callJson();
+    callJson2();
 })
+
+
--- js/policy-details.js
+++ js/policy-details.js
@@ -206,4 +206,12 @@ $(document).ready(function () {
         console.log($this.closest('.loc-grid-table').index())
     })
 
+
+    $('input[type="radio"]').keydown(function (e) {
+        var arrowKeys = [37, 38, 39, 40];
+        if (arrowKeys.indexOf(e.which) !== -1) {
+            $(this).blur();
+            return false;
+        }
+    });
 })
